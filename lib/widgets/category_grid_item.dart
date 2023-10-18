bf7b0e38c71258406a365a9ca999070262b85df5
ggkangkang <choonkhang1998@gmail.com>
2023-10-18 20:49:27 +0800

    first commit

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
